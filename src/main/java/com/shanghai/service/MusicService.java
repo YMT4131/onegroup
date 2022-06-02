d6158c83743297ec58fbb53aff722b661cb0f9ba
fcj <fcj3124@163.com>
2022-06-02 09:19:13 +0800

    方成杰004

--- src/main/java/com/shanghai/service/MusicService.java
+++ src/main/java/com/shanghai/service/MusicService.java
@@ -8,7 +8,10 @@ import com.shanghai.dao.MusicMapper;
 import com.shanghai.model.MusicModel;
 import com.shanghai.po.Music;
 import com.shanghai.query.MusicQuery;
+import com.shanghai.utils.AssertUtil;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.HashMap;
@@ -32,10 +35,23 @@ public class MusicService extends BaseService<Music,Integer> {
         //得到对应的分页对象
         PageInfo<MusicModel> pageInfo = new PageInfo<>(
                 musicMapper.searchMusic(musicQuery));
-
+        //分页对象设置到返回值中
         layerTableModel.setCount(pageInfo.getTotal());
         layerTableModel.setData(pageInfo.getList());
-
         return layerTableModel;
+    }
+
+    /**
+     *  执行删除操作-实际上是更改isValid的值，绑定的是行工具栏右侧的
+     *  1.判断musicId非空
+     *  2.调用dao层方法，判断受影响行数
+     * @param musicId
+     * @return
+     */
+    @Transactional(propagation = Propagation.REQUIRED)
+    public void deleteOneMusic(Integer musicId){
+        AssertUtil.isTrue(musicId == null ||
+                musicMapper.selectByPrimaryKey(musicId) == null,"所删除的记录不存在！");
+        AssertUtil.isTrue(musicMapper.deleteByPrimaryKey(musicId) != 1,"删除失败！");
     }
 }
