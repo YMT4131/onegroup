93a6ba8aca14ca0d470724c65985de83e99d1395
pj <your_email@domain.com>
2022-06-02 10:05:39 +0800

    pj

---
+++ src/main/java/com/shanghai/model/TypeModel.java
@@ -0,0 +1,49 @@
+package com.shanghai.model;
+
+public class TypeModel {
+    private Integer typeId;
+    private String typeName;
+    private String typeInfo;
+    private String typeSong;
+    private String typeMenu;
+
+    public Integer getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(Integer typeId) {
+        this.typeId = typeId;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public String getTypeInfo() {
+        return typeInfo;
+    }
+
+    public void setTypeInfo(String typeInfo) {
+        this.typeInfo = typeInfo;
+    }
+
+    public String getTypeSong() {
+        return typeSong;
+    }
+
+    public void setTypeSong(String typeSong) {
+        this.typeSong = typeSong;
+    }
+
+    public String getTypeMenu() {
+        return typeMenu;
+    }
+
+    public void setTypeMenu(String typeMenu) {
+        this.typeMenu = typeMenu;
+    }
+}
