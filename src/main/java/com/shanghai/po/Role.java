9a7c86698c3f47c5c22ca7fd5da0308f8f686cf1
zys <1131292645@qq.com>
2022-06-06 20:52:59 +0800

    zys 001

--- src/main/java/com/shanghai/po/Role.java
+++ src/main/java/com/shanghai/po/Role.java
@@ -1,5 +1,7 @@
 package com.shanghai.po;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.util.Date;
 
 public class Role {
@@ -8,9 +10,9 @@ public class Role {
     private String roleName;
 
     private String roleRemark;
-
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createDate;
-
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date updateDate;
 
     private Integer isValid;
