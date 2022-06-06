3faaca2d40ba9a64c6a230d3519f99ef56bfbdb3
Janz1z7 <963674558@qq.com>
2022-06-06 20:59:35 +0800

    supei 用户管理增删改查

--- src/main/java/com/shanghai/controller/IndexController.java
+++ src/main/java/com/shanghai/controller/IndexController.java
@@ -2,7 +2,6 @@ package com.shanghai.controller;
 
 import com.shanghai.base.BaseController;
 import com.shanghai.base.ResultInfo;
-import com.shanghai.po.User;
 import com.shanghai.po.vo.UserModel;
 import com.shanghai.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
